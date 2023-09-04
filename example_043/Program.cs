6180a7897f96b079e185b614e28d33304495ae4c
Dmitry <sicily007@rambler.ru>
2023-09-04 22:54:25 +0300

    final_commit

--- example_043/Program.cs
+++ example_043/Program.cs
@@ -4,3 +4,23 @@
 
 b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
 */
+
+IntersectionPoint();
+
+void IntersectionPoint()
+{
+System.Console.WriteLine("Введите через Enter значения b1, k1, b2, k2");
+double b1 = double.Parse(Console.ReadLine());
+double k1 = double.Parse(Console.ReadLine());
+double b2 = double.Parse(Console.ReadLine());
+double k2 = double.Parse(Console.ReadLine());
+if (k1 == k2 && b1 == b2) System.Console.WriteLine("Прямая лежит в прямой!");
+else if (k1 == k2 && b1 != b2) System.Console.WriteLine("Прямые параллельны!");
+else if (k1 != k2 && b1 == b2) System.Console.WriteLine("Точка пересечения в нуле");
+else
+{
+    double resultX = (b2 - b1) / (k1 - k2);
+    double resultY = k1 * resultX + b1;
+    System.Console.WriteLine($"Прямые пересекутся в X: {resultX}, Y: {resultY}");
+}
+}
